a74c20249fa0bc4b896b68698bb3c485aefa0c83
lukeeff <luke4nandez@gmail.com>
2023-01-26 16:34:30 -0800

    convert ammo class to struct

--- Source/CapstoneProject/Public/AmmoState.h
+++ Source/CapstoneProject/Public/AmmoState.h
@@ -3,39 +3,20 @@
 #include "CoreMinimal.h"
 #include "AmmoState.generated.h"
 
-UCLASS(BlueprintType)
-class UAmmoState : public UObject
+USTRUCT(BlueprintType)
+struct FAmmoState
 {
 	GENERATED_BODY()
 
 public:
-    UAmmoState();
-    UAmmoState(int32 NewAmmoCurrent, int32 NewAmmoMax, int32 NewClipSize);
+    FAmmoState();
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintGetter=GetAmmoCurrent, BlueprintSetter=SetAmmoCurrent)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
     int32 AmmoCurrent;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintGetter=GetAmmoMax, BlueprintSetter=SetAmmoMax)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
     int32 AmmoMax;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintGetter=GetClipSize, BlueprintSetter=SetClipSize)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
     int32 ClipSize;
-
-    UFUNCTION(BlueprintCallable, BlueprintGetter)
-    int32 GetAmmoCurrent();
-
-    UFUNCTION(BlueprintCallable, BlueprintSetter)
-    void SetAmmoCurrent(int32 NewAmmoCurrent);
-
-    UFUNCTION(BlueprintCallable, BlueprintGetter)
-    int32 GetAmmoMax();
-
-    UFUNCTION(BlueprintCallable, BlueprintSetter)
-    void SetAmmoMax(int32 NewAmmoMax);
-
-    UFUNCTION(BlueprintCallable, BlueprintGetter)
-    int32 GetClipSize();
-
-    UFUNCTION(BlueprintCallable, BlueprintSetter)
-    void SetClipSize(int32 NewClipSize);
 };
