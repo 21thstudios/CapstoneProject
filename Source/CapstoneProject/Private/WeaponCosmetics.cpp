0be28f274a2acc81acc2e96d1edb65e7f591bb8d
lukeeff <luke4nandez@gmail.com>
2023-01-26 16:38:27 -0800

    convert cosmetics class to struct

--- Source/CapstoneProject/Private/WeaponCosmetics.cpp
+++ Source/CapstoneProject/Private/WeaponCosmetics.cpp
@@ -1,58 +1,8 @@
 
 #include "WeaponCosmetics.h"
 #include "../CapstoneProject.h"
-#include "Animation/AnimSequence.h"
-#include "Sound/SoundBase.h"
 
-UWeaponCosmetics::UWeaponCosmetics()
+FWeaponCosmetics::FWeaponCosmetics()
 {
-}
 
-UWeaponCosmetics::UWeaponCosmetics(UAnimSequence* NewAttackAnimation, UAnimSequence* NewReloadAnimation, TArray<USoundBase*> NewAttackSounds, TArray<USoundBase*> NewReloadSounds)
-{
-    this->AttackAnimation = NewAttackAnimation;
-    this->ReloadAnimation = NewReloadAnimation;
-    this->AttackSounds = NewAttackSounds;
-    this->ReloadSounds = NewReloadSounds;
-}
-
-UAnimSequence* UWeaponCosmetics::GetAttackAnimation()
-{
-    return this->AttackAnimation;
-}
-
-
-void UWeaponCosmetics::SetAttackAnimation(UAnimSequence* NewAttackAnimation)
-{
-    this->AttackAnimation = NewAttackAnimation;
-}
-
-UAnimSequence* UWeaponCosmetics::GetReloadAnimation()
-{
-    return this->ReloadAnimation;
-}
-
-void UWeaponCosmetics::SetReloadAnimation(UAnimSequence* NewReloadAnimation)
-{
-    this->ReloadAnimation = NewReloadAnimation;
-}
-
-TArray<USoundBase*> UWeaponCosmetics::GetAttackSounds()
-{
-    return this->AttackSounds;
-}
-
-void UWeaponCosmetics::SetAttackSounds(TArray<USoundBase*> NewAttackSounds)
-{
-    this->AttackSounds = NewAttackSounds;
-}
-
-TArray<USoundBase*> UWeaponCosmetics::GetReloadSounds()
-{
-    return this->ReloadSounds;
-}
-
-void UWeaponCosmetics::SetReloadSounds(TArray<USoundBase*> NewReloadSounds)
-{
-    this->ReloadSounds = NewReloadSounds;
 }
--- Source/CapstoneProject/Public/WeaponCosmetics.h
+++ Source/CapstoneProject/Public/WeaponCosmetics.h
@@ -3,48 +3,23 @@
 #include "CoreMinimal.h"
 #include "WeaponCosmetics.generated.h"
 
-UCLASS(BlueprintType)
-class CAPSTONEPROJECT_API UWeaponCosmetics : public UObject
+USTRUCT(BlueprintType)
+struct FWeaponCosmetics
 {
 	GENERATED_BODY()
 
 public:
-	UWeaponCosmetics();
-	UWeaponCosmetics(UAnimSequence* NewAttackAnimation, UAnimSequence* NewReloadAnimation, TArray<USoundBase*> NewAttackSounds, TArray<USoundBase*> NewReloadSounds);
+	FWeaponCosmetics();
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintGetter=GetAttackAnimation, BlueprintSetter=SetAttackAnimation)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UAnimSequence* AttackAnimation;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintGetter=GetReloadAnimation, BlueprintSetter=SetReloadAnimation)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UAnimSequence* ReloadAnimation;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintGetter=GetAttackSounds, BlueprintSetter=SetAttackSounds)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TArray<USoundBase*> AttackSounds;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, BlueprintGetter=GetReloadSounds, BlueprintSetter=SetReloadSounds)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TArray<USoundBase*> ReloadSounds;
-
-	UFUNCTION(BlueprintCallable, BlueprintGetter)
-    UAnimSequence* GetAttackAnimation();
-
-	UFUNCTION(BlueprintCallable, BlueprintSetter)
-    void SetAttackAnimation(UAnimSequence* NewAttackAnimation);
-
-	UFUNCTION(BlueprintCallable, BlueprintGetter)
-    UAnimSequence* GetReloadAnimation();
-
-	UFUNCTION(BlueprintCallable, BlueprintSetter)
-    void SetReloadAnimation(UAnimSequence* NewReloadAnimation);
-
-	UFUNCTION(BlueprintCallable, BlueprintGetter)
-    TArray<USoundBase*> GetAttackSounds();
-
-	UFUNCTION(BlueprintCallable, BlueprintSetter)
-    void SetAttackSounds(TArray<USoundBase*> NewAttackSounds);
-
-	UFUNCTION(BlueprintCallable, BlueprintGetter)
-    TArray<USoundBase*> GetReloadSounds();
-
-	UFUNCTION(BlueprintCallable, BlueprintSetter)
-    void SetReloadSounds(TArray<USoundBase*> NewReloadSounds);
 };
