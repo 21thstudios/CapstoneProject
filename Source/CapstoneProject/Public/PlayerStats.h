1ee7754aaa4f08e92a57c7f33cdf05e01e04d280
Joey S <joeystenbeck@gmail.com>
2023-02-02 11:42:37 -0800

    Add PlayerStats class which has PlayerKills, PlayerDeaths, and getters, setters, and incrementers for each.

---
+++ Source/CapstoneProject/Public/PlayerStats.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerStats.generated.h"
+
+UCLASS(BlueprintType)
+/**
+ * 
+ */
+class CAPSTONEPROJECT_API UPlayerStats : public UObject
+{
+  GENERATED_BODY()
+
+public:
+	UPlayerStats();
+	~UPlayerStats();
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  int PlayerKills;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  int PlayerDeaths;
+
+  UFUNCTION(BlueprintCallable)
+  void AddKill();
+  UFUNCTION(BlueprintCallable)
+  void AddDeath();
+
+  // getter and setter for each 
+  UFUNCTION(BlueprintCallable)
+  int GetPlayerKills();
+  UFUNCTION(BlueprintCallable)
+  int GetPlayerDeaths();
+  UFUNCTION(BlueprintCallable)
+  void SetPlayerKills(int Kills);
+  UFUNCTION(BlueprintCallable)
+  void SetPlayerDeaths(int Deaths);
+};
