ca961a0665b0f765828d16358f3c40344bc0ab85
Joey S <joeystenbeck@gmail.com>
2023-02-17 11:39:58 -0800

    Copy over PlayerStats.cpp from PlayerStats branch.

---
+++ Source/CapstoneProject/Private/PlayerStats.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerStats.h"
+#include "../CapstoneProject.h"
+
+UPlayerStats::UPlayerStats()
+{
+  this->PlayerKills = 0;
+  this->PlayerDeaths = 0;
+}
+
+UPlayerStats::~UPlayerStats()
+{
+}
+
+void UPlayerStats::AddKill()
+{
+  this->PlayerKills++;
+}
+
+void UPlayerStats::AddDeath()
+{
+  this->PlayerDeaths++;
+}
+
+void UPlayerStats::HandleDeath(UPlayerStats* Actor)
+{
+  this->AddDeath();
+  Actor->AddKill();
+}
+
+void UPlayerStats::SetPlayerKills(int Kills)
+{
+  this->PlayerKills = Kills;
+}
+
+void UPlayerStats::SetPlayerDeaths(int Deaths)
+{
+  this->PlayerDeaths = Deaths;
+}
+
+int UPlayerStats::GetPlayerKills()
+{
+  return this->PlayerKills;
+}
+
+int UPlayerStats::GetPlayerDeaths()
+{
+  return this->PlayerDeaths;
+}
