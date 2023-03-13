626ec795d6e078c0b5630b61e3e0e60d98105a1e
lukeeff <luke4nandez@gmail.com>
2023-03-13 11:07:14 -0700

    implement LAN mode checkbox option for session list

--- Source/CapstoneProject/Public/SessionList.h
+++ Source/CapstoneProject/Public/SessionList.h
@@ -1,17 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/CheckBox.h"
 #include "SessionList.generated.h"
 
-/**
- * 
- */
+
+
 UCLASS()
 class CAPSTONEPROJECT_API USessionList : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public :
+	void SetLAN(bool LAN);
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UCheckBox* LANCheckBox;
 };
