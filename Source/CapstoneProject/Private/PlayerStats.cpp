4c50033cb26aff1194f78f787f97497816899117
Joseph Stenbeck <stenbejo@oregonstate.edu>
2023-03-13 11:24:06 -0700

    Copy in PlayerStats class from other branch.

---
+++ Source/CapstoneProject/Private/PlayerStats.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+// Source: https://forums.unrealengine.com/t/print-to-screen-using-c/357351/3 
+#define D(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT(x));}
+
+#include "PlayerStats.h"
+#include "../CapstoneProject.h"
+
+UPlayerStats::UPlayerStats()
+{
+  this->PlayerKills = 0;
+  this->PlayerDeaths = 0;
+}
+
+UPlayerStats::~UPlayerStats()
+{
+}
+
+void UPlayerStats::AddKill()
+{
+  this->PlayerKills++;
+}
+
+void UPlayerStats::AddDeath()
+{
+  this->PlayerDeaths++;
+}
+
+void UPlayerStats::HandleDeath(UPlayerStats* Actor)
+{
+  this->AddDeath();
+  Actor->AddKill();
+  auto PlayerNamea = Actor->GetPlayerName().ToString();
+  D("Player " + PlayerNamea + " killed " + this->GetPlayerName().ToString() + "!");
+}
+
+void UPlayerStats::SetPlayerKills(int Kills)
+{
+  this->PlayerKills = Kills;
+}
+
+void UPlayerStats::SetPlayerDeaths(int Deaths)
+{
+  this->PlayerDeaths = Deaths;
+}
+
+int UPlayerStats::GetPlayerKills()
+{
+  return this->PlayerKills;
+}
+
+int UPlayerStats::GetPlayerDeaths()
+{
+  return this->PlayerDeaths;
+}
+
+FName UPlayerStats::GetPlayerName()
+{
+  return this->PlayerName;
+}
+
+void UPlayerStats::SetPlayerName(FName Name)
+{
+  this->PlayerName = Name;
+}
---
+++ Source/CapstoneProject/Public/PlayerStats.h
@@ -0,0 +1,43 @@
+#include "PlayerStats.generated.h"
+
+
+UCLASS(BlueprintType)
+/**
+ * 
+ */
+class CAPSTONEPROJECT_API UPlayerStats : public UObject
+{
+  GENERATED_BODY()
+
+public:
+	UPlayerStats();
+	~UPlayerStats();
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  int PlayerKills;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  int PlayerDeaths;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite)
+  FName PlayerName;
+
+  UFUNCTION(BlueprintCallable)
+  void AddKill();
+  UFUNCTION(BlueprintCallable)
+  void AddDeath();
+  UFUNCTION(BlueprintCallable)
+  void HandleDeath(UPlayerStats* Actor);
+
+  // getter and setter for each 
+  UFUNCTION(BlueprintCallable)
+  int GetPlayerKills();
+  UFUNCTION(BlueprintCallable)
+  int GetPlayerDeaths();
+  UFUNCTION(BlueprintCallable)
+  void SetPlayerKills(int Kills);
+  UFUNCTION(BlueprintCallable)
+  void SetPlayerDeaths(int Deaths);
+  UFUNCTION(BlueprintCallable)
+  void SetPlayerName(FName Name);
+  UFUNCTION(BlueprintCallable)
+  FName GetPlayerName();
+};
