12e6747f5c8a3f98d7ef60aa5e543d4494f4d3f1
lukeeff <luke4nandez@gmail.com>
2023-03-13 16:13:19 -0700

    implement HostSession cpp

--- Source/CapstoneProject/Private/MainMenuScreen.cpp
+++ Source/CapstoneProject/Private/MainMenuScreen.cpp
@@ -3,6 +3,10 @@
 
 #include "MainMenuScreen.h"
 
+#include "OnlineSessionSettings.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+
 
 void UMainMenuScreen::NativeConstruct()
 {
@@ -19,7 +23,34 @@ void UMainMenuScreen::NativeDestruct()
 bool UMainMenuScreen::HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN,
                                   bool bIsPresence, int32 MaxNumPlayers)
 {
-	
+	if (IOnlineSubsystem* const OnlineSubsystem = IOnlineSubsystem::Get())
+	{
+		IOnlineSessionPtr Session = OnlineSubsystem->GetSessionInterface();
+		if (Session.IsValid() && UserId.IsValid())
+		{
+			SessionSettings = MakeShareable(new FOnlineSessionSettings());
+			SessionSettings->bIsLANMatch = bIsLAN;
+			SessionSettings->bUsesPresence = bIsPresence;
+			SessionSettings->NumPublicConnections = MaxNumPlayers;
+			SessionSettings->NumPrivateConnections = 0;
+			SessionSettings->bAllowInvites = true;
+			SessionSettings->bAllowJoinInProgress = true;
+			SessionSettings->bShouldAdvertise = true;
+			SessionSettings->bAllowJoinViaPresence = true;
+			SessionSettings->bAllowJoinViaPresenceFriendsOnly = true;
+
+			SessionSettings->Set(SETTING_MAPNAME, FString("NewMap"), EOnlineDataAdvertisementType::ViaOnlineService);
+
+			OnCreateSessionCompleteDelegateHandle = Session->AddOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegate);
+
+			// Create session delegate triggered upon completion
+			return Session->CreateSession(*UserId, SessionName, *SessionSettings);
+		}
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, TEXT("No OnlineSubsytem found!"));
+	}
 }
 
 void UMainMenuScreen::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
