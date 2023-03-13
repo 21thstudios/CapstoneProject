045729ddcfef57664d45d07dedfa1b649c4ef629
Joey S <joeystenbeck@gmail.com>
2023-03-13 09:28:15 -0700

    Temp.

--- Source/CapstoneProject/Private/InGamePlayerState.cpp
+++ Source/CapstoneProject/Private/InGamePlayerState.cpp
@@ -1,3 +1,5 @@
+
+/*
 // Fill out your copyright notice in the Description page of Project Settings.
 
 // Source: https://forums.unrealengine.com/t/print-to-screen-using-c/357351/3 
@@ -14,9 +16,15 @@ AInGamePlayerState::AInGamePlayerState()
     this->PlayerDeaths = 0;
 }
 
-void AInGamePlayerState::HandleKill()
+void AInGamePlayerState::HandleDeath(AInGamePlayerState* OtherState)
 {
-  
+    this->AddDeath();
+    Actor->AddKill();
+    auto PlayerNameTemp = Actor->GetPlayerName().ToString();
+    D("Player " + PlayerNameTemp + " killed " + this->GetPlayerName().ToString() + "!");
+}
+{
+
 }
 
 void AInGamePlayerState::ClearPlayerState()
@@ -24,3 +32,20 @@ void AInGamePlayerState::ClearPlayerState()
     this->PlayerKills = 0;
     this->PlayerDeaths = 0;
 }
+
+///////////
+UPlayerStats::UPlayerStats()
+{
+  this->PlayerKills = 0;
+  this->PlayerDeaths = 0;
+}
+
+void UPlayerStats::HandleDeath(UPlayerStats* Actor)
+{
+  this->AddDeath();
+  Actor->AddKill();
+  auto PlayerNameTemp = Actor->GetPlayerName().ToString();
+  D("Player " + PlayerNameTemp + " killed " + this->GetPlayerName().ToString() + "!");
+}
+
+*/
--- Source/CapstoneProject/Public/InGamePlayerState.h
+++ Source/CapstoneProject/Public/InGamePlayerState.h
@@ -1,14 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+/*
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
 #include "InGamePlayerState.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class CAPSTONEPROJECT_API AInGamePlayerState : public APlayerState
 {
@@ -41,3 +38,4 @@ public:
     UFUNCTION(BlueprintCallable)
     void SetPlayerDeaths(int Deaths);
 };
+*/
