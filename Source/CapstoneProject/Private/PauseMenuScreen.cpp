9e50ca3d15b7c7fad661e96cd149dff731d12be2
lukeeff <luke4nandez@gmail.com>
2023-03-14 16:32:57 -0700

    implement OnClickesumeGameButton functionality

--- Source/CapstoneProject/Private/PauseMenuScreen.cpp
+++ Source/CapstoneProject/Private/PauseMenuScreen.cpp
@@ -3,19 +3,26 @@
 
 #include "PauseMenuScreen.h"
 
+#include "Components/Button.h"
+
 void UPauseMenuScreen::NativeConstruct()
 {
 	Super::NativeConstruct();
+	ResumeGameButton->OnClicked.AddDynamic(this, &UPauseMenuScreen::OnClickResumeGameButton);
+	QuitToTitleButton->OnClicked.AddDynamic(this, &UPauseMenuScreen::OnClickQuitToTitleButton);
 }
 
 void UPauseMenuScreen::NativeDestruct()
 {
-	
+	Super::NativeDestruct();
 }
 
 void UPauseMenuScreen::OnClickResumeGameButton()
 {
-	
+	APlayerController* PlayerController = GetOwningPlayer();
+	PlayerController->SetInputMode(FInputModeGameOnly());
+	PlayerController->SetShowMouseCursor(false);
+	this->RemoveFromParent();
 }
 
 void UPauseMenuScreen::OnClickQuitToTitleButton()
