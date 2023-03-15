9db14876956d08ebe7ad0063d984578f04436da8
lukeeff <luke4nandez@gmail.com>
2023-03-14 17:44:44 -0700

    create GameInstance class for as future session manager

---
+++ Source/CapstoneProject/Public/SessionGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SessionGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONEPROJECT_API USessionGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
