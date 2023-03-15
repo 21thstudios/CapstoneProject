5120f7dbefcd7b3e4bfd7645edad51c1031536a3
lukeeff <luke4nandez@gmail.com>
2023-03-14 17:50:58 -0700

    wip extract cpp header stubs to session game instance

--- Source/CapstoneProject/Public/SessionGameInstance.h
+++ Source/CapstoneProject/Public/SessionGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionDelegates.h"
 #include "SessionGameInstance.generated.h"
 
 /**
@@ -13,5 +14,20 @@ UCLASS()
 class CAPSTONEPROJECT_API USessionGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	USessionGameInstance(const FObjectInitializer& ObjectInitializer);
 	
+	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN, bool bIsPresence, int32 MaxNumPlayers);
+
+	virtual void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
+	
+private:	
+	FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
+	FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
+	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
+	FDelegateHandle OnStartSessionCompleteDelegateHandle;
+
+	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
 };
