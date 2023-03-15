69a7ec5eb397d503c7489a018ff6e11c01521010
lukeeff <luke4nandez@gmail.com>
2023-03-14 18:34:18 -0700

    improve readability of SessionGameInstance header

--- Source/CapstoneProject/Public/SessionGameInstance.h
+++ Source/CapstoneProject/Public/SessionGameInstance.h
@@ -8,7 +8,7 @@
 #include "SessionGameInstance.generated.h"
 
 /**
- * 
+ * Extension of the GameInstance responsible for Sessions-related work.
  */
 UCLASS()
 class CAPSTONEPROJECT_API USessionGameInstance : public UGameInstance
@@ -17,17 +17,27 @@ class CAPSTONEPROJECT_API USessionGameInstance : public UGameInstance
 
 public:
 	USessionGameInstance(const FObjectInitializer& ObjectInitializer);
-	
-	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN, bool bIsPresence, int32 MaxNumPlayers);
 
+	/** Creating online sessions */
+	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN, bool bIsPresence, int32 MaxNumPlayers);
 	virtual void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
 	void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
+
+	/** Finding online sessions */
+	void FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence);
+	void OnFindSessionsComplete(bool bWasSuccessful);
 	
-private:	
+private:
+	/** Creating online session delegates */
 	FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
 	FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
 	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
-	FDelegateHandle OnStartSessionCompleteDelegateHandle;
+    FDelegateHandle OnStartSessionCompleteDelegateHandle;
 
+	/** Finding online session delegates */
+	FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
+	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
+	
 	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
 };
