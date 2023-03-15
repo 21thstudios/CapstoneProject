a763e2a4e775eeaa6811d71e260e2d13ecc82b6e
lukeeff <luke4nandez@gmail.com>
2023-03-14 18:04:51 -0700

    implement HostSession interface for game instance

--- Source/CapstoneProject/Private/SessionGameInstance.cpp
+++ Source/CapstoneProject/Private/SessionGameInstance.cpp
@@ -3,9 +3,50 @@
 
 #include "SessionGameInstance.h"
 
+#include "OnlineSessionSettings.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+#include "Online/OnlineSessionNames.h"
+
+const FString HOST_MAP_DESTINATION_NAME = TEXT("FirstPersonMap");
+
 USessionGameInstance::USessionGameInstance(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
 {
 	OnCreateSessionCompleteDelegate = FOnCreateSessionCompleteDelegate::CreateUObject(this, &USessionGameInstance::OnCreateSessionComplete);
 	OnStartSessionCompleteDelegate = FOnCreateSessionCompleteDelegate::CreateUObject(this, &USessionGameInstance::OnStartOnlineGameComplete);
+}
+
+bool USessionGameInstance::HostSession(TSharedPtr<const FUniqueNetId> UserId,FName SessionName,
+	bool bIsLAN, bool bIsPresence, int32 MaxNumPlayers)
+{
+	if (IOnlineSubsystem* const OnlineSubsystem = IOnlineSubsystem::Get())
+	{
+		IOnlineSessionPtr Session = OnlineSubsystem->GetSessionInterface();
+		if (Session.IsValid() && UserId.IsValid())
+		{
+			SessionSettings = MakeShareable(new FOnlineSessionSettings());
+			SessionSettings->bIsLANMatch = bIsLAN;
+			SessionSettings->bUsesPresence = bIsPresence;
+			SessionSettings->NumPublicConnections = MaxNumPlayers;
+			SessionSettings->NumPrivateConnections = 5;
+			SessionSettings->bAllowInvites = true;
+			SessionSettings->bAllowJoinInProgress = true;
+			SessionSettings->bShouldAdvertise = true;
+			SessionSettings->bAllowJoinViaPresence = true;
+			SessionSettings->bAllowJoinViaPresenceFriendsOnly = true;
+			SessionSettings->Set(SETTING_MAPNAME, HOST_MAP_DESTINATION_NAME, EOnlineDataAdvertisementType::ViaOnlineService);
+
+			OnCreateSessionCompleteDelegateHandle = Session->AddOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegate);
+			
+			UE_LOG(LogTemp, Display, TEXT("Session Interface is valid. Attempting to create session..."))
+			return Session->CreateSession(*UserId, SessionName, *SessionSettings);
+		}
+		UE_LOG(LogTemp, Error, TEXT("Unable to initiate session creation due to invalid Session Interface or UserId"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Unable to initiate session creation due to uninitialized Online Subsystem!"));
+	}
+	return false;
 }
