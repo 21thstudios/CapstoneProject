0a469c2019c6e0b23648ffd18506f3b7e700e807
Joey Stenbeck <joeystenbeck@gmail.com>
2023-03-16 18:04:12 -0700

    Add debug print message to ensure that game is using new player state.

--- Source/CapstoneProject/Private/CPP_PlayerState.cpp
+++ Source/CapstoneProject/Private/CPP_PlayerState.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) 21thStudios contributors. All rights reserved. Licensed under the MIT license.
 
+// Source: https://forums.unrealengine.com/t/print-to-screen-using-c/357351/3 
+#define D(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT(x));}
 
 #include "CPP_PlayerState.h"
 
@@ -8,5 +10,6 @@ ACPP_PlayerState::ACPP_PlayerState()
 	this->Kills = 0;
 	this->Deaths = 0;
 	this->Name = "a";
+	D("scree");
 }
 
