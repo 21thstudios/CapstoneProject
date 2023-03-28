f6c5471043e22ea2b986ba79c96272008a78429f
lukeeff <luke4nandez@gmail.com>
2023-03-28 12:11:54 -0700

    create widget for scoreboard

---
+++ Source/CapstoneProject/Public/ScoreboardWidget.h
@@ -0,0 +1,18 @@
+// Copyright (c) 21thStudios contributors. All rights reserved. Licensed under the MIT license.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ScoreboardWidget.generated.h"
+
+/**
+ * Base user widget for scoreboards. Provided a list of PlayerState and visualizes the information within it to the
+ * end user.
+ */
+UCLASS()
+class CAPSTONEPROJECT_API UScoreboardWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
