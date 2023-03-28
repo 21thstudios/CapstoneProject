d717ae88994990d385fd5d6cbb1e0e4be7232df7
lukeeff <luke4nandez@gmail.com>
2023-03-28 15:10:38 -0700

    implement setservername

--- Source/CapstoneProject/Private/ScoreboardWidget.cpp
+++ Source/CapstoneProject/Private/ScoreboardWidget.cpp
@@ -12,3 +12,19 @@ void UScoreboardWidget::SetMapName(FText MapName) const
 		MapNameTextBlock->SetText(MapName);
 	}
 }
+
+void UScoreboardWidget::SetServerName(FText ServerName) const
+{
+	if (ServerNameTextBlock)
+	{
+		FFormatNamedArguments Args;
+		Args.Add("Prefix", *TEXT("Server"));
+		Args.Add("ServerName", ServerName);
+		FText FormattedText = FText::Format(
+			NSLOCTEXT("Scoreboard", "ServerNameWithPrefixFormat", "{Prefix}: {ServerName}"),
+			Args);
+		
+		ServerNameTextBlock->SetText(FormattedText);
+	}
+	UE_LOG(LogTemp, Error, TEXT("Failed to set server name of scoreboard due to invalid ServerNameTextBlock!"));
+}
