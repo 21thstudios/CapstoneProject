86c63542a4283e9e9fa6f6a9689bfc02cbd5e15e
lukeeff <luke4nandez@gmail.com>
2023-03-28 12:16:13 -0700

    create widtget for scoreboard entries

---
+++ Source/CapstoneProject/Public/ScoreboardEntryWidget.h
@@ -0,0 +1,17 @@
+// Copyright (c) 21thStudios contributors. All rights reserved. Licensed under the MIT license.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ScoreboardEntryWidget.generated.h"
+
+/**
+ * Base user widget for scoreboard entries. This is the part of the scoreboard which contains individual player data. 
+ */
+UCLASS()
+class CAPSTONEPROJECT_API UScoreboardEntryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
