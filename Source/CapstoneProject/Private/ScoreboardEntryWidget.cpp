05011a836992c920ba7fd48dd4c38439882131e3
lukeeff <luke4nandez@gmail.com>
2023-03-28 16:15:00 -0700

    implement SetPingInMs

--- Source/CapstoneProject/Private/ScoreboardEntryWidget.cpp
+++ Source/CapstoneProject/Private/ScoreboardEntryWidget.cpp
@@ -13,6 +13,24 @@ void UScoreboardEntryWidget::SetPlayerDisplayName(FText DisplayName)
 	}
 	else
 	{
-		UE_LOG(LogTemp, Error, TEXT("Failed to set server name of scoreboard entry due to invalid PlayerDisplayNameTextBlock!"));
+		UE_LOG(LogTemp, Error, TEXT("Failed to set name of scoreboard entry due to invalid PlayerDisplayNameTextBlock!"));
+	}
+}
+
+void UScoreboardEntryWidget::SetPingInMs(int32 Ping)
+{
+	if (PingMsTextBlock)
+	{
+		FFormatNamedArguments Args;
+		Args.Add("PingInMs", Ping);
+		Args.Add("ms", *TEXT("ms"));
+		FText FormattedText = FText::Format(
+			NSLOCTEXT("ScoreboardEntry", "PingInMsFormat", "{PingInMs}{ms}"),
+			Args);
+		PingMsTextBlock->SetText(FormattedText);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to set ping of scoreboard entry due to invalid PingMsTextBlock!"));
 	}
 }
