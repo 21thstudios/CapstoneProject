4e7089c79f19b99e88e3ffb7645ae48b7c3229ca
Joey Stenbeck <joeystenbeck@gmail.com>
2023-04-07 13:56:46 -0700

    Remove unneeded comments and debug prints.

--- Source/CapstoneProject/Private/CPP_PlayerState.cpp
+++ Source/CapstoneProject/Private/CPP_PlayerState.cpp
@@ -8,18 +8,16 @@
 #include "Net/UnrealNetwork.h"
 #include "Engine/World.h"
 #include <string>
+
+#include "../../../../../../../../Program Files/Epic Games/UE_5.1/Engine/Platforms/Hololens/Source/Runtime/Core/Public/Microsoft/MinWindowsPrivate.h"
 #define Dnum(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, std::to_string(x).c_str());}
 #define DFstr(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, x);}
 
 ACPP_PlayerState::ACPP_PlayerState()
 {
-	// Inheriting here and adding instance variables feels not modular. Any recommendations
-	// for whether to continue doing it this way, or maybe extract these variables into another
-	// class/some other approach? UE encourages doing it this way. 
 	this->Kills = 0;
 	this->Deaths = 0;
-	// Temporary solution here-- we would need to extend our API to get a proper player name,
-	// so we're using the player ID in string form. 
+	// Replace with proper way to get player name once available 
 	this->Name = std::to_string(this->GetUniqueID()).c_str();
 	this->bReplicates = true;
 }
@@ -46,32 +44,11 @@ void ACPP_PlayerState::KillOtherPlayer(ACPP_PlayerState* OtherPlayer)
 {
 	this->Kills++;
 	OtherPlayer->Deaths++;
-	// Debug message to ensure that the player killing and the player being killed
-	// are not the same. 
-	if (OtherPlayer == this)
-	{
-		D("OtherPlayer is self");
-	}
-
 	FString KillerName = this->Name.ToString();
-	FString DierName = OtherPlayer->Name.ToString();
-	FString DisplayMessage = KillerName + " killed " + DierName + "!";
-
+	FString DeathName = OtherPlayer->Name.ToString();
+	FString DisplayMessage = KillerName + " killed " + DeathName + "!";
 	DFstr(DisplayMessage);
-
-	// More debug. 
-	// this->PrintStatsOnScreen();
-	// OtherPlayer->PrintStatsOnScreen();
-
 	ACPP_GameState* GameState = GetWorld()->GetGameState<ACPP_GameState>();
-	// const char* mode = GameState->mode; 
-	// If we're playing in the mode where the game ends once a certain number of kills
-	// are reached, execute this code (definitely not an ideal solution). It also feels
-	// weird to grab a `GameState` reference from a `PlayerState`. I don't see anything
-	// immediately wrong with it, but I'm curious for any advice/suggestions. 
-	// if (strcmp(mode, "kills") == 0)
-	// {
-	// }
 	if (this->Kills > GameState->kills_to_end)
 	{
 		GameState->ResetAllPlayersStates();
@@ -96,7 +73,6 @@ void ACPP_PlayerState::PrintStatsOnScreen()
 	this->PrintDeathsOnScreen();
 }
 
-// Source: https://forums.unrealengine.com/t/replicating-variables-in-c/79772/2 
 void ACPP_PlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
