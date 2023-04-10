9179fbe4fa060d27ea222ce9649437ecc293ee81
Joey Stenbeck <joeystenbeck@gmail.com>
2023-04-10 12:44:33 -0700

    Remove dead comment.

--- Source/CapstoneProject/Private/CPP_PlayerState.cpp
+++ Source/CapstoneProject/Private/CPP_PlayerState.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) 21thStudios contributors. All rights reserved. Licensed under the MIT license.
 
-// Source: https://forums.unrealengine.com/t/print-to-screen-using-c/357351/3 
 #define D(x) if(GEngine){GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT(x));}
 
 #include "CPP_PlayerState.h"
